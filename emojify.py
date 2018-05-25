9a8476fa2c27d2429fa08dd385a4ae00f440e6fa
Hugo Fragata <hugofragata@ua.pt>
2018-05-25 23:23:29 +0100

    added a few files  🅰️

---
+++ emojify.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import json
+from random import random
+
+
+def emojify(c):
+    f = open('emoji.json')
+    d = json.load(f)
+    p = c.split(' ')
+    es = []
+    for e in d:
+        for w in p:
+            if w in e['tags']:
+                es.append(e['emoji'])
+            if w in e['aliases']:
+                es.append(e['emoji'])
+    if es != []:
+        for e in es:
+            c += ' '+e
+    else:
+        c += ' '+d[int(random() * (len(d)-1))]['emoji']+' '+d[int(random() * (len(d)-1))]['emoji']
+    return c
+
+
+def append_to_file():
+    with open(sys.argv[1], 'r') as message_file:
+        lines = message_file.readlines()
+        lines[0] = emojify(lines[0])
+    with open(sys.argv[1], 'w') as message_file:
+        message_file.write(''.join(lines).encode('utf-8'))
+
+
+if __name__ == '__main__':
+    append_to_file()
---
+++ install.sh
@@ -0,0 +1,3 @@
+cp emoji.json ./.git/hooks/
+cp emojify.py ./.git/hooks/prepare-commit-msg
+chmod +x ./.git/hooks/prepare-commit-msg
